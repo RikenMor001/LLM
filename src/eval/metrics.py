511c57249158aa7c7b53b98163351b066d2167f2
Riken Patel <riken9377@gmail.com>
2026-06-03 16:16:29 -0400

    metrics calculation donw

--- src/eval/metrics.py
+++ src/eval/metrics.py
@@ -10,6 +10,10 @@ class Metrics:
     def update(self, step:int, loss: float):
         self.losses.append(loss)
         self.steps.append(step)
+        
+        if len(self.losses) > self.window:
+            self.losses.pop(0)
+            self.steps.pop(0)
 
     def calculate_avg_loss(self):
         calculated_loss = sum(self.losses) / len(self.losses)
@@ -18,3 +22,25 @@ class Metrics:
         else:
             return calculated_loss
 
+    # what is ppl?
+    # ppl stands for perplexity, is a measure of how well a model 
+    # predicts the next token in sequence
+    def get_ppl(self):
+        avg_loss = self.calculate_avg_loss()
+        if avg_loss is None:
+            return None
+        else: 
+            return math.exp(avg_loss)
+
+    def log(self, step: int):
+        avg_loss = self.calculate_avg_loss()
+        ppl = self.get_ppl()
+
+        if avg_loss is None:
+            return None
+
+        print(
+            f"Step: {step} "
+            f"Loss: {avg_loss:.4f} | "
+            f"PPL: {ppl:.2f}"
+        )

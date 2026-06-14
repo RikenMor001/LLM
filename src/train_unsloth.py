fe3863b747032c3c3d8e5029fcd38f5c91bc6cb2
Riken Patel <riken9377@gmail.com>
2026-06-13 21:05:44 -0400

    trained unsloth, not tested

---
+++ src/train_unsloth.py
@@ -0,0 +1,17 @@
+# training the unsloth model that is already been coded
+from models.unsloth_loader import load_model, add_lora
+from trl import SFTTrainer
+from transformers import TrainingArguments
+
+model, tokenizer = load_model()
+model = add_lora(model)
+
+trainer = SFTTrainer(
+    model = model, 
+    train_dataset = [{"text":open("input.txt").read()}],
+    args = TrainingArguments(
+        output_dir = "outputs",
+        num_train_epochs = 1
+    )
+    trainer.train()
+)

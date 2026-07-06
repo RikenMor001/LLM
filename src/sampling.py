499df0aeee3ef251bcf8cb3a51da2120fc186d01
Riken Patel <riken9377@gmail.com>
2026-07-05 22:18:15 -0400

    applying top_k

--- src/sampling.py
+++ src/sampling.py
@@ -35,6 +35,7 @@ def apply_repetition_penalty(
     
     return logits
 
+# temperature means the variance of the distribution, higher temperature means more variance
 def apply_temperature(
     logits: torch.Tensor,
     temperature: float,
@@ -42,3 +43,15 @@ def apply_temperature(
     if temperature <= 0:
         return logits
     return logits / temperature
+
+# top_k means a filter, so I choose the top most quantity of k and the ditribution shouldnt be more than that
+
+def apply_top_k(
+    logits: torch.Tensor,
+    top_k: int,
+    min_tokens_to_keep: int
+) -> torch.Tensor:
+    k = min(max(min_tokens_to_keep, top_k), logits.size(-1))
+    values, _ = torch.topk(logits, k, dim=-1)
+    cutoff = values[:, [-1]]
+    return logits.masked_fill(logits < cutoff, float('-inf'))
